e2f6df40d16c631f24f42dc269775e84fa08a31b
Jessica Henry <S00214115@mail.itsligo.ie>
2022-04-19 14:34:34 +0100

    breakchange test

--- InsuranceWebApp/index.aspx.cs
+++ InsuranceWebApp/index.aspx.cs
@@ -33,7 +33,7 @@ namespace InsuranceWebApp
                 if ((age >= 18) && (age <= 30))
                     premium = 5.0;
                 else
-                    if (age  31 )
+                    if (age 31)
                     premium = 2.50;
                 else
                     premium = 0.0;

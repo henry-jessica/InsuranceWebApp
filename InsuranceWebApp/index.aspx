da91e4a068812d53a10bc061082987f15c0778cd
Jessica Henry <S00214115@mail.itsligo.ie>
2022-04-19 14:09:47 +0100

    some changes

--- InsuranceWebApp/index.aspx
+++ InsuranceWebApp/index.aspx
@@ -8,14 +8,17 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <h1>JJ Insurance Company</h1>
+        <h2>JJ Insurance Company</h2>
        <asp:Label ID="lblAge" runat="server" Text="Age"></asp:Label><br/>
-        <asp:TextBox ID="txbAge" runat="server"></asp:TextBox><br/>
+        <asp:TextBox ID="txbAge" runat="server"></asp:TextBox>
+        <br />
+        <br/>
         <asp:Label ID="Label2" runat="server" Text="Location"></asp:Label><br/>
         <asp:TextBox ID="tbxLocation" runat="server"></asp:TextBox><br/><br/>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <asp:Button ID="Button1" runat="server" Text="Calcule" OnClick="Button1_Click" />
         <br /><br /><br />
-        premium: <asp:Label ID="lblResult" runat="server" Text="0"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Premium: <asp:Label ID="lblResult" runat="server" Text="0"></asp:Label>
 
     </form>
     
--- InsuranceWebApp/index.aspx.cs
+++ InsuranceWebApp/index.aspx.cs
@@ -7,10 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace InsuranceWebApp
 {
-   
+
     public partial class InsuranceForm : System.Web.UI.Page
     {
-       
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -19,7 +19,7 @@ namespace InsuranceWebApp
         protected void Button1_Click(object sender, EventArgs e)
         {
 
-            CalcPremium(int.Parse(txbAge.Text), tbxLocation.Text); 
+            CalcPremium(int.Parse(txbAge.Text), tbxLocation.Text);
 
             lblResult.Text = (CalcPremium(int.Parse(txbAge.Text), tbxLocation.Text)).ToString();
 
@@ -30,44 +30,27 @@ namespace InsuranceWebApp
             double premium;
 
             if (location == "rural")
-            {
                 if ((age >= 18) && (age <= 30))
-                {
                     premium = 5.0;
-                }
-                else if (age >= 31)
-                {
+                else
+                    if (age >= 31)
                     premium = 2.50;
-                }
                 else
-                {
                     premium = 0.0;
-                }
-            }
-            else if (location == "urban")
-            {
-                if ((age >= 18) && (age <= 35))
-                {
-                    premium = 6.0;
-                }
-                else if (age >= 36)
-                {
-                    premium = 5.0;
-                }
-                else
-                {
-                    premium = 0.0;
-                }
-            }
             else
-            {
+    if (location == "urban")
+                if ((age >= 18) && (age <= 35))
+                    premium = 6.0;
+                else
+                   if (age >= 36)
+                    premium = 5.0;
+                else
+                    premium = 0.0;
+            else
                 premium = 0.0;
-            }
 
             if (age >= 50)
-            {
                 premium = premium * 0.15;
-            }
             return premium;
 
         }
